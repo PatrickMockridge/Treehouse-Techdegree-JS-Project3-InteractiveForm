0c40ce5784a688fb4ce5db5ab16f5e6dc34e4166
Patrick Mockridge <patrick@engzig.com>
2016-05-04 19:21:08 +0100

    Step 5 Complete
    Completed up to and including Step 5

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ $(this).attr("id", "fieldset-" + (index+1));
 $('#fieldset-1').append("<input type='text' id='other-field' placeholder='Your Title...' name='otherJob'>");
 //hide text field initially
 $("#other-field").hide();
+//empty the color selector
+$('#color').empty();
 // html finished loading, focus on first input field
 //focus on first field when document is ready document ready
 $(document).ready(function() {
@@ -22,3 +24,23 @@ $( "#title").change(function() {
  else {
     $("#other-field").hide();
   }});
+  $( "#title").change(function() {
+    if ($("#title option:selected").text() == "Other") {
+        $("#other-field").show();
+  }
+   else {
+      $("#other-field").hide();
+    }});
+
+$( "#design").change(function() {
+      console.log($("#design option:selected").text())
+      if ($("#design option:selected").text() == "Theme - JS Puns") {
+          $("#color").html("<option value='cornflowerblue'>Cornflower Blue</option><option value='darkslategrey'>Dark Slate Grey</option><option value='gold'>Gold</option>")
+          }
+      else if ($("#design option:selected").text() == 'Theme - I ♥ JS') {
+              $("#color").html("<option value='tomato'>Tomato</option><option value='steelblue'>Steel Blue</option><option value='dimgrey'>Dim Grey</option>")
+          }
+      else {
+              $('#color').empty();
+      }
+});
